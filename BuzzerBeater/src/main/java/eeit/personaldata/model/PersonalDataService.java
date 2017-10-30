fcf09b1365bb17e463f8ff775611cb4a45a85210
李紹群 <aaa@aaa>
2017-10-30 16:45:19 +0800

    修改datatable為中文
    新增單場數據

--- BuzzerBeater/src/main/java/eeit/personaldata/model/PersonalDataService.java
+++ BuzzerBeater/src/main/java/eeit/personaldata/model/PersonalDataService.java
@@ -2,25 +2,33 @@ package eeit.personaldata.model;
 
 import java.util.List;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class PersonalDataService {
 	private PersonalDataDAO_interface dao;
 
 	public PersonalDataService() {
-		dao = new PersonalDataDAO();
+		@SuppressWarnings("resource")
+		ApplicationContext context = new ClassPathXmlApplicationContext("modelConfig2_JNDI.xml");
+		dao = (PersonalDataDAO_interface) context.getBean("PersonalDataDAO");
 	}
 
 	// 給個人數據的查詢
 	public List<PersonalDataVO> getAll() {
+		dao = new PersonalDataDAO();
 		return dao.getAll();
 	}
 
 	// 給team的查詢
 	public List<PersonalDataVO> getAll1() {
+		dao = new PersonalDataDAO();
 		return dao.getAll1();
 	}
 
 	// 給後台使用PersonalData的新刪修
 	public List<PersonalDataVO> getAll2() {
+		dao = new PersonalDataDAO();
 		return dao.getAll2();
 	}
 
